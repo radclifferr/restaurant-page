5d848a686f61fdfb5732de98bd30573b8045ca4f
Richard Radcliffe <radcliffer94@gmail.com>
2022-10-16 14:08:36 -0700

    Add contact page

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,32 @@
 export default createContactPage;
+import servingFood from "./images/man-serving-food.jpg";
 
-function createContactPage () {
-    console.log("hi")
+function createContactPage (container) {
+    const footer= document.querySelector(".footer");
+    const body = document.createElement("div");
+    body.classList.add("main");
+    container.insertBefore(body, footer);
+
+    const contactPage = document.createElement("div");
+    contactPage.classList.add("contact-page");
+    body.appendChild(contactPage);
+
+
+    const detailLeft = document.createElement("img");
+    detailLeft.classList.add("detailLeft");
+    detailLeft.src = servingFood;
+    contactPage.appendChild(detailLeft);
+
+
+    const detailRight = document.createElement("div");
+    detailRight.classList.add("detailRight");
+    contactPage.appendChild(detailRight);
+
+    const detailRightCopy = ["Contact Us To Place An Order!", "Phone (555) 555-5555", "fakeEmail@reallyFakeEmail.com"]
+    for (let i = 0; i <detailRightCopy.length; i++){
+        const copyDiv = document.createElement("div");
+        copyDiv.textContent = detailRightCopy[i];
+        detailRight.appendChild(copyDiv);
+    }
+
 }
--- src/homepage.js
+++ src/homepage.js
@@ -1,14 +1,26 @@
 export default createHomePage;
+import restaurant from "./images/resturant.jpg";
+import featuredPlate from "./images/featured-plate.jpg";
+import moreFood from "./images/more-food.jpg";
 
 function createHomePage (container) {
     const footer= document.querySelector(".footer");
     const copy = ["Welcome to our Resturant! Good food Here for you to eat!","Featured plate, good grubs", "Chow Down on this. I promise it wont kill you. "]
+    const copyImages = [restaurant, featuredPlate, moreFood];
     const body = document.createElement("div");
     body.classList.add("main");
     container.insertBefore(body, footer);
+
+    const homePage = document.createElement("div");
+    homePage.classList.add("home-page");
+    body.appendChild(homePage);
+
     for (let i=0; i< copy.length; i++){
         const text = document.createElement("div");
         text.textContent= copy[i];
-        body.appendChild(text);
+        homePage.appendChild(text);
+        const image = document.createElement("img");
+        image.src = copyImages[i];
+        text.appendChild(image);
     }
 }
