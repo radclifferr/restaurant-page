e7e0bab8c6e446c6b9a902a3a2f5f53bdb3288ce
Richard Radcliffe <radcliffer94@gmail.com>
2022-10-16 15:22:33 -0700

    Style menu and contact page with resized images

--- src/menu.js
+++ src/menu.js
@@ -9,22 +9,35 @@ import pizzaCake from "./images/pizza-cake.jpg";
 
 
 function createMenuPage(container) {
-    const menuItems = ["Sushi Doughnuts", "Grilled Cheese Doughnuts", "Black Charcoal Bun Lobster Rolls", "Succulent Cupcakes", "Pizza Cake"];
+    const menuItems = ["Sushi Doughnuts - $44ea", "Grilled Cheese Doughnuts - $15ea", "Black Charcoal Bun Lobster Rolls - $29ea", "Succulent Cupcakes - $Dont Ask", "Pizza Cake - $16"];
 
     const menuImages = [sushiDoughnut, grilledCheeseDoughnut, lobsterRoll, cupcake, pizzaCake];
+    const menuDescription = ["A delectate dance for your tastebuds","For 'those days'", "Order this to impress your friends", "A crowd favorite among plant lovers", "Pizza Cake, need I say more?"]
     const footer= document.querySelector(".footer");
     const body = document.createElement("div");
     body.classList.add("main");
     container.insertBefore(body, footer);
+
+    const menu =document.createElement("div");
+    menu.classList.add("menu");
+    body.appendChild(menu);
+
+
     for(let i=0; i< menuItems.length; i++){
         const menuItem = document.createElement("div");
         menuItem.classList.add("menu-item");
         menuItem.textContent = menuItems[i];
-        body.appendChild(menuItem);
+        menu.appendChild(menuItem);
 
         const menuPictures = document.createElement("img");
         menuPictures.src = menuImages[i];
         menuItem.appendChild(menuPictures);
+
+        const menuDescriptors = document.createElement ("div");
+        menuDescriptors.textContent = menuDescription[i];
+        menuItem.appendChild(menuDescriptors);
+
+
     }
 
 
